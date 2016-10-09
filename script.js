06114af34817e75405fa7905a3673b7b94e5138a
Miranda Motsinger <mirandamotsinger@email.arizona.edu>
2016-10-08 22:05:10 -0700

    changes to code & images

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 var app = angular.module("idleIdol", []);
 
 app.controller("idolController", function($scope) {
+	OVERTIME_PAY = 20;
+	$scope.IDOL_COST = 200;
+
 	$scope.cash = 0;
+	$scope.starPoints = 0;
 	$scope.idols = [];
+	$scope.idolCapacity = 3;
+
+	$scope.industryPartyEnabled = true;
+	$scope.workOvertimeEnabled = true;
 
 	var moneyMultiplier = 1;
+
+	console.log($scope.idols.length);
 
 	setInterval (update, 100);
 
@@ -13,26 +23,50 @@ app.controller("idolController", function($scope) {
 	}
 
 	$scope.randomIdol = function randomIdol() {
-		if(Math.random() > 0.5) {
+		rand = Math.random();
+		if(rand > 0.66) {
 			$scope.idols.push("sponge.png");
+		} else if(rand < 0.66 && rand > 0.33) {
+			$scope.idols.push("garbage.png");
 		} else {
 			$scope.idols.push("hategirl.png");
 		}
-
-		$scope.cash -= 100;
+		console.log($scope.idols.length);
+		removeCash($scope.IDOL_COST);
 	}
 
 	$scope.workOvertime = function workOvertime() {
-		$scope.cash += 20;
+		$scope.workOvertimeEnabled = false;
+		setTimeout(function() {
+			$scope.workOvertimeEnabled = true;
+			addCash(OVERTIME_PAY);
+		}, 1000);
+	}
+
+	$scope.industryParty = function industryParty() {
+		removeCash(40);
+		$scope.industryPartyEnabled = false;
+		setTimeout(function() {
+			addSP (1);
+			$scope.industryPartyEnabled = true;
+		}, 1000);
 	}
 
 	function update () {
 	    addCash (moneyMultiplier);
+	   	$scope.$apply();
 	}
 
 	function addCash (cashToAdd) {
 	    $scope.cash += cashToAdd;
-	    $scope.$apply();
+	}
+
+	function removeCash (cashToRemove) {
+		$scope.cash -= cashToRemove;
+	}
+
+	function addSP (starPointsToAdd) {
+		$scope.starPoints += starPointsToAdd;
 	}
 
 	function roundDec (number) {
